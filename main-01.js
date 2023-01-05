6c8d3bb4e3962bc2e0e421850a74eae396a6d58c
PedroBataglia <114232124+PedroBataglia@users.noreply.github.com>
2023-01-05 09:01:16 -0300

    Primeiro dia concluído com sucesso

---
+++ main-01.js
@@ -0,0 +1,24 @@
+let numeroUm = 1
+let stringUm = '1'
+let numeroTrinta = 30
+let stringTrinta ='30'
+let numeroDez = 10
+let stringDez = '10'
+
+if ( numeroUm == stringUm) {
+    console.log(`As variáveis numeroUm e stringUM  são tem o mesmo valor, porém são de tipos diferentes`)  
+} else {
+    console.log('As variáveis numeroUm e stringUM tem valores diferentes e são de tipos diferentes.')
+}
+
+if (numeroTrinta === stringTrinta) {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor e são do mesmo tipo')
+} else {
+    console.log('As variáveis numeroTrinta e stringTrinta tem o mesmo valor, porém são de tipos diferentes.')
+}
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroDez e stringDez tem o mesmo valor, mas são de tipos diferentes')
+} else {
+    console.log('As variáveis numeroDez e stringDez tem valores diferentes e são de tipos diferentes.')
+}
